e7b8d9974484e4bcd26cf62707d7b6b2860c7bfa
Sanjay <sanjayraops17@gmail.com>
2025-09-17 20:53:25 +0530

    MOG

--- Practice/palindrome.py
+++ Practice/palindrome.py
@@ -1,7 +1,9 @@
+#BAsic code to check for the palindrome of a string.
 def pali(s):
     if s == s[::-1]:
         return True
     return False
 
 s=input("Enter a string to check for palindrome:")
+
 print(pali(s))
