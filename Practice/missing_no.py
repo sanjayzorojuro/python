c99f06c16391c6c66c5aab41fa96c24e434d86e9
Sanjay <sanjayraops17@gmail.com>
2025-09-18 21:23:55 +0530

    Update missing_no.py

--- Practice/missing_no.py
+++ Practice/missing_no.py
@@ -1,6 +1,7 @@
 '''This is a program where it returns the missing number of n.
 for example if the list element is [2,3,4,1] and n = 5 then the missing term in the list is 5.
 another example lst=[10,9,8,7,6,5,3,2,1] and here the n would be 10 and the missing number is 4 so the program should return 4 as the answer.
+the numbers in the list should be within the value of n.
 '''
 def missing(n):
     lst=[]
@@ -15,4 +16,5 @@ def missing(n):
 
 n=int(input("Enter the length of the element:"))
 print(missing(n))
+
 
